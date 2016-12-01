46362653598fad9cc2c5fafe288635d66a9ba169
huoshan12345 <89009143@qq.com>
2016-12-02 00:28:33 +0800

    begin wiser.net

---
+++ src/Wiser.Net/PostingsList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wiser.Net
+{
+    public class PostingsList
+    {
+        public int DocumentId { get; set; }             /* 文档编号 */
+        public List<int> Positions { get; set; }        /* 位置信息的数组 */
+        public int PositionsCount { get; set; }        /* 位置信息的条数 */
+        public PostingsList Next { get; set; }/* 指向下一个倒排列表的指针 */
+    }
+}
